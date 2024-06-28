ef2020c8c8818c3bbd99800ab9f3d7b07b6ee2a5
NDzaky <ndzakyie@gmail.com>
2024-06-29 01:15:27 +0700

    Add files via upload

---
+++ kuis04.js
@@ -0,0 +1,19 @@
+var text = "";
+
+for (var i = 0; i <= 0; i++){
+    text += "*";
+}
+
+text += "\n";
+
+for (var a = 2; a <= 5; a++){
+    for (var b = 5 - 1; b >= a; b--){
+        text += " ";
+    }
+    for (var c = 1; c <= a; c++){
+        text += "*";
+    }
+    text += "\n";
+}
+
+console.log(text);
